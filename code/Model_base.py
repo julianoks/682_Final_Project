12a06172e23ede5e3ba627e420a60551abcb5882
Julian Oks <skoj824@gmail.com>
2017-12-08 16:29:53 -0500

    Update Model_base.py

--- code/Model_base.py
+++ code/Model_base.py
@@ -74,10 +74,10 @@ class Classifier_Model(object):
 		for i in range(iterations):
 			batch = mnist_dataset.train.next_batch(batch_size)
 			var, grad = zipped[int(rand_state.rand() * len(zipped))]
-			#var_mask = tf.cast(rand_state.rand(*var.shape)<update_percent, var.dtype)
-			var_mask = np.zeros(shape=var.shape)
-			for _ in range(3): var_mask[:,:,:,int(rand_state.rand()*int(var.shape[-1]))] = 1
-			var_mask = tf.cast(var_mask, var.dtype)
+			var_mask = tf.cast(rand_state.rand(*var.shape)<update_percent, var.dtype)
+			#var_mask = np.zeros(shape=var.shape)
+			#for _ in range(3): var_mask[:,:,:,int(rand_state.rand()*int(var.shape[-1]))] = 1
+			#var_mask = tf.cast(var_mask, var.dtype)
 			var_grad, batch_accuracy = self.sess.run([grad, accuracy], feed_dict={X: batch[0], Y: batch[1]})
 			history.append(batch_accuracy)
 			if (print_every != False) and (i % print_every == 0):
