96a3aa7d2d7e13f75c12704db1a5092c1fd296ef
Julian Oks <skoj824@gmail.com>
2017-12-08 16:18:27 -0500

    Update Model_base.py

--- code/Model_base.py
+++ code/Model_base.py
@@ -75,7 +75,7 @@ class Classifier_Model(object):
 			var, grad = zipped[int(rand_state.rand() * len(zipped))]
 			#var_mask = tf.cast(rand_state.rand(*var.shape)<update_percent, var.dtype)
 			var_mask = tf.zeros(shape=var.shape)
-			for _ in range(3): var_mask[:,:,:,int(rand_state.rand()*int(var.shape[-1]))]
+			for _ in range(3): var_mask[:,:,:,int(rand_state.rand()*int(var.shape[-1]))] = 1
 			var_grad, batch_accuracy = self.sess.run([grad, accuracy], feed_dict={X: batch[0], Y: batch[1]})
 			history.append(batch_accuracy)
 			if (print_every != False) and (i % print_every == 0):
